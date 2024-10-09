1624ffbc28d2f0bbe6309c3ae36e20265a444c08
Jamel-Travieso <115836192+Jamel-Travieso@users.noreply.github.com>
2024-10-09 16:18:06 -0600

    @Repository

--- carniceria/src/main/java/com/bendicion/la/carniceria/carniceria/jpa/RolRepository.java
+++ carniceria/src/main/java/com/bendicion/la/carniceria/carniceria/jpa/RolRepository.java
@@ -4,12 +4,14 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 /**
  *
  * @author Jamel Sandí
  */
 
+@Repository
 public interface RolRepository extends JpaRepository<Rol, Integer>{
     
       // SP Add
--- carniceria/src/main/java/com/bendicion/la/carniceria/carniceria/jpa/UsuarioRepository.java
+++ carniceria/src/main/java/com/bendicion/la/carniceria/carniceria/jpa/UsuarioRepository.java
@@ -5,12 +5,14 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 /**
  *
  * @author Jamel Sandí
  */
 
+@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
     
     // SP Add 
