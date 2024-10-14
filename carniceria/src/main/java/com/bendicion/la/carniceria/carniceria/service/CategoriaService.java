b32134d3dda1489cf0d2aa71ea4a7a627f5d5269
Glend <glendrojas1040@gmail.com>
2024-10-14 02:19:04 -0600

    Update CategoriaService.java
    Crud completo, falta que sea bonito

--- carniceria/src/main/java/com/bendicion/la/carniceria/carniceria/service/CategoriaService.java
+++ carniceria/src/main/java/com/bendicion/la/carniceria/carniceria/service/CategoriaService.java
@@ -1,10 +1,17 @@
 package com.bendicion.la.carniceria.carniceria.service;
-import com.bendicion.la.carniceria.carniceria.domain.Categoria;
-import com.bendicion.la.carniceria.carniceria.jpa.CategoriaRepository;
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
+
+import com.bendicion.la.carniceria.carniceria.domain.Categoria;
+import com.bendicion.la.carniceria.carniceria.jpa.CategoriaRepository;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.ParameterMode;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.StoredProcedureQuery;
 
 /**
  *
@@ -17,6 +24,10 @@ public class CategoriaService implements ICategoriaService {
 
     @Autowired
     private CategoriaRepository categoriaRep;
+
+
+     @PersistenceContext
+    private EntityManager entityManager;
     
     // Aquí en vez de llamar el mae .save se llama el SP (igual co los demás)
     
@@ -27,12 +38,31 @@ public class CategoriaService implements ICategoriaService {
         return categoria;
     }
 
-    @Override
+     @Override
     public Categoria updateCategoria(Categoria categoria) {
-        System.out.println("Actualizando categoría con ID: " + categoria.getIdCategoria());
-        categoriaRep.updateProcedureCategoria(categoria.getIdCategoria(), categoria.getNombreCategoria(), categoria.getDescripcionCategoria());
+
+
+        try {
+            StoredProcedureQuery query = entityManager.createStoredProcedureQuery("spActualizarCategoria");
+            query.registerStoredProcedureParameter(1, Integer.class, ParameterMode.IN);
+            query.registerStoredProcedureParameter(2, String.class, ParameterMode.IN);
+            query.registerStoredProcedureParameter(3, String.class, ParameterMode.IN);
+
+            query.setParameter(1, categoria.getIdCategoria());
+            query.setParameter(2, categoria.getNombreCategoria());
+            query.setParameter(3, categoria.getDescripcionCategoria());
+           
+            
+
+            query.execute();
+        } catch (Exception e) {
+            System.err.println("Error al ejecutar el procedimiento almacenado: " + e.getMessage());
+            throw e;
+        }
+
         return categoria;
     }
+
 
     @Override
     public List<Categoria> getCategoria() {
