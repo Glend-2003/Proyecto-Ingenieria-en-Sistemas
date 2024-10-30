7db40956277d7391fb7f94d20753bd25e4905ee6
Glend <glendrojas1040@gmail.com>
2024-10-30 13:21:19 -0600

    index

--- front/src/index.js
+++ front/src/index.js
@@ -1,36 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import './index.css';
 import LoginApp from './components/Login/LoginApp';
 import Principal from './components/principal';
 import SideBar from './components/SideBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Navigate } from 'react-router-dom';
 import Registrar from './components/Usuarios/Registrar';
 import Categoria from './components/Categoria/CategoriaApp';
 import Usuarios from './components/Usuarios/GestionarUsuario';
 
-// Componente para proteger rutas
+// Componente para proteger rutas y mostrar el sidebar si el usuario está autenticado
 const PrivateRoute = ({ children }) => {
   const token = localStorage.getItem('token');
-  return token ? children : <Navigate to="/" />;
+  return token ? (
+    <div className="main-container">
+      <SideBar /> {/* Sidebar solo visible si el usuario está autenticado */}
+      <div className="main-content">
+        {children}
+      </div>
+    </div>
+  ) : (
+    <Navigate to="/" />
+  );
 };
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Router>
-    <div className="main-container">
-      <SideBar /> {/* Sidebar siempre visible */}
-      <div className="main-content">
-        <Routes>
-          <Route path="/" element={<LoginApp />} />
-          <Route path="/principal" element={<PrivateRoute><Principal /></PrivateRoute>} />
-          <Route path="/register" element={<Registrar />} />
-          <Route path="/CategoriaApp" element={<PrivateRoute><Categoria /></PrivateRoute>} />
-          <Route path="/GestionarUsuario" element={<PrivateRoute><Usuarios /></PrivateRoute>} />
-        </Routes>
-      </div>
-    </div>
+    <Routes>
+      <Route path="/" element={<LoginApp />} />
+      <Route path="/principal" element={<PrivateRoute><Principal /></PrivateRoute>} />
+      <Route path="/register" element={<Registrar />} />
+      <Route path="/CategoriaApp" element={<PrivateRoute><Categoria /></PrivateRoute>} />
+      <Route path="/GestionarUsuario" element={<PrivateRoute><Usuarios /></PrivateRoute>} />
+    </Routes>
   </Router>
 );
