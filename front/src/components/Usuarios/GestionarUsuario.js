252d8ff85747b69209f478cd7dab6e1673e755f5
Glend <glendrojas1040@gmail.com>
2024-10-30 13:52:00 -0600

    cambios front

--- front/src/components/Usuarios/GestionarUsuario.js
+++ front/src/components/Usuarios/GestionarUsuario.js
@@ -1,22 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { FaCheck, FaTrash, FaExclamationTriangle } from 'react-icons/fa';
-import { toast, ToastContainer } from 'react-toastify'; // Importar toast y ToastContainer
+import { FaKey, FaTrash } from 'react-icons/fa';
+import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Swal from 'sweetalert2';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCheck, faTrash, faExclamationTriangle, faEdit } from '@fortawesome/free-solid-svg-icons';
+import { faEdit } from '@fortawesome/free-solid-svg-icons';
 import useAuth from '../../hooks/useAuth';
 import Navbar from '../Navbar';
 import { Button, Modal } from 'react-bootstrap';
 
 const GestionarUsuario = () => {
-  // Estado para manejar los datos de los usuarios
   const [users, setUsers] = useState([]);
   const [search, setSearch] = useState('');
   const [userEdit, setUserEdit] = useState(null);
   const [showModal, setShowModal] = useState(false);
+  const [showPasswordModal, setShowPasswordModal] = useState(false);
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
   const { usuario, handleLogout } = useAuth();
   const [currentPage, setCurrentPage] = useState(1);
   const itemsPerPage = 5;
@@ -25,17 +27,6 @@ const GestionarUsuario = () => {
     cargarUsuarios();
   }, []);
 
-  // Función para manejar el cambio de los inputs de la tabla
-  const handleInputChange = (e, idUsuario, field) => {
-    const newValue = e.target.value;
-    setUsers((prevUsers) =>
-      prevUsers.map((user) =>
-        user.idUsuario === idUsuario ? { ...user, [field]: newValue } : user
-      )
-    );
-  };
-
-  // Función para obtener la lista de usuarios desde el backend
   const cargarUsuarios = async () => {
     try {
       const response = await axios.get('http://localhost:8080/usuario/');
@@ -46,79 +37,47 @@ const GestionarUsuario = () => {
     }
   };
 
-  // Función para agregar un usuario
   const agregarUsuario = async (usuario) => {
-    // Validación de correo duplicado
-    const correoDuplicado = users.some(
-      (user) => user.correoUsuario.toLowerCase() === usuario.correoUsuario.toLowerCase()
-    );
-
-    if (correoDuplicado) {
-      toast.error('El correo del usuario ya existe. Por favor, elige uno diferente.');
-      return;
-    }
-
     try {
-      await axios.post('http://localhost:8080/usuario/registrar', usuario);
-      console.log('Usuario agregado:', usuario);
-      toast.success('Usuario agregado con éxito', {
-        autoClose: 1500,
-        onClose: () => {
-          cargarUsuarios();
-        }
-      });
+      await axios.post('http://localhost:8080/usuario/agregar', usuario);
+      toast.success('Usuario agregado con éxito');
+      cargarUsuarios();
+      handleCloseModal();
     } catch (error) {
-      console.error('Error al agregar usuario:', error);
+      console.error('Error al agregar el usuario:', error);
       toast.error('Ocurrió un error al agregar el usuario');
     }
   };
 
-  // Función para actualizar un usuario
   const actualizarUsuario = async (usuario) => {
-    // Validación de correo duplicado al actualizar
-    const correoDuplicado = users.some(
-      (user) =>
-        user.correoUsuario.toLowerCase() === usuario.correoUsuario.toLowerCase() &&
-        user.idUsuario !== usuario.idUsuario
-    );
-
-    if (correoDuplicado) {
-      toast.error('El correo del usuario ya existe. Por favor, elige uno diferente.', {
-        autoClose: 1500,
-        onClose: () => {
-          cargarUsuarios();
-        },
-      });
-      return;
-    }
-
-    const { isConfirmed } = await Swal.fire({
-      title: '¿Estás seguro?',
-      text: 'Esta acción no se puede deshacer.',
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonText: 'Sí, actualizar',
-      cancelButtonText: 'No, cancelar',
-      reverseButtons: true,
-    });
-
-    if (isConfirmed) {
-      try {
-        await axios.put('http://localhost:8080/usuario/actualizar', usuario);
-        toast.success('Usuario actualizado con éxito', {
-          autoClose: 1500,
-          onClose: () => {
-            cargarUsuarios();
-          },
-        });
-      } catch (error) {
-        console.error('Error al actualizar usuario:', error);
-        toast.error('Ocurrió un error al actualizar el usuario');
-      }
+    try {
+      await axios.put('http://localhost:8080/usuario/actualizar', usuario);
+      toast.success('Usuario actualizado con éxito');
+      cargarUsuarios();
+      handleCloseModal();
+    } catch (error) {
+      console.error('Error al actualizar usuario:', error);
+      toast.error('Ocurrió un error al actualizar el usuario');
     }
   };
 
-  // Función para eliminar un usuario
+  //FALTA METODO BACKEND
+  const actualizarContrasena = async (idUsuario) => {
+    if (password !== confirmPassword) {
+      toast.error('Las contraseñas no coinciden');
+      return;
+    }
+    try {
+      await axios.put(`http://localhost:8080/usuario/actualizarContrasena`, { idUsuario, password });
+      toast.success('Contraseña actualizada con éxito');
+      cargarUsuarios();
+      handleClosePasswordModal();
+    } catch (error) {
+      console.error('Error al actualizar la contraseña:', error);
+      toast.error('Ocurrió un error al actualizar la contraseña');
+    }
+  };
+
   const handleDelete = async (idUsuario) => {
     const { isConfirmed } = await Swal.fire({
       title: '¿Estás seguro?',
@@ -130,57 +89,51 @@ const GestionarUsuario = () => {
       reverseButtons: true,
     });
 
-    if (!isConfirmed) {
-      return;
-    }
+    if (!isConfirmed) return;
 
     try {
-      const response = await axios.delete(`http://localhost:8080/usuario/eliminar/${idUsuario}`);
-      if (response.status === 200) {
-        toast.success('Usuario eliminado con éxito', {
-          autoClose: 1500,
-          onClose: () => {
-            cargarUsuarios();
-          },
-        });
-      }
+      await axios.delete(`http://localhost:8080/usuario/eliminar/${idUsuario}`);
+      toast.success('Usuario eliminado con éxito');
+      cargarUsuarios();
     } catch (error) {
       console.error('Error al eliminar usuario:', error);
-      if (error.response) {
-        console.error('Detalles del error:', error.response.data);
-      }
       toast.error('Ocurrió un error al eliminar el usuario');
     }
   };
 
-  // Función para manejar la búsqueda
-  const handleSearchChange = (e) => {
-    setSearch(e.target.value);
+  const handleShowPasswordModal = (user) => {
+    setUserEdit(user);
+    setShowPasswordModal(true);
   };
 
-  // Función para mostrar el modal
+  const handleClosePasswordModal = () => {
+    setShowPasswordModal(false);
+    setPassword('');
+    setConfirmPassword('');
+  };
+
+  const handleSearchChange = (e) => setSearch(e.target.value);
+
   const handleShowModal = (user = null) => {
     setUserEdit(user);
     setShowModal(true);
   };
+
   const handleCloseModal = () => {
     setShowModal(false);
     setUserEdit(null);
   };
 
-  // Filtrar los usuarios según la búsqueda
   const filteredUsers = users.filter(user =>
     user.nombreUsuario.toLowerCase().includes(search.toLowerCase())
   );
 
-   // Paginación
-   const totalPages = Math.ceil(filteredUsers.length / itemsPerPage);
-   const indexOfLastItem = currentPage * itemsPerPage;
-   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-   const currentCategorias = filteredUsers.slice(indexOfFirstItem, indexOfLastItem);
- 
-   const paginate = (pageNumber) => setCurrentPage(pageNumber);
-   
+  const totalPages = Math.ceil(filteredUsers.length / itemsPerPage);
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentCategorias = filteredUsers.slice(indexOfFirstItem, indexOfLastItem);
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
   return (
     <div>
@@ -188,37 +141,35 @@ const GestionarUsuario = () => {
       <div className="container mt-5">
         <h1>Gestión de Usuarios</h1>
         <Button variant="primary" onClick={() => handleShowModal()}>Agregar Nuevo Usuario</Button>
+        
+        {/* Modal de Actualización */}
         <Modal show={showModal} onHide={handleCloseModal}>
           <Modal.Header closeButton>
             <Modal.Title>{userEdit && userEdit.idUsuario ? 'Actualizar Usuario' : 'Agregar Usuario'}</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            <form className="user" onSubmit={(e) => {
+            <form onSubmit={(e) => {
               e.preventDefault();
               if (userEdit && userEdit.idUsuario) {
                 actualizarUsuario(userEdit);
               } else {
-                console.log('userEdit:', userEdit);
                 agregarUsuario(userEdit);
               }
-              handleCloseModal();
             }}>
               <div className="mb-3">
                 <input
-                  className="form-control form-control-user"
+                  className="form-control"
                   type="email"
-                  id="email"
                   placeholder="Correo Electrónico"
                   required
                   value={userEdit ? userEdit.correoUsuario : ''}
                   onChange={(e) => setUserEdit({ ...userEdit, correoUsuario: e.target.value })}
-                  style={{ borderColor: '#d7d7d7', color: '#212529' }}
                 />
               </div>
               <div className="row mb-3">
-                <div className="col-sm-6 col-md-4 mb-3 mb-sm-0">
+                <div className="col-sm-6">
                   <input
-                    className="form-control form-control-user"
+                    className="form-control"
                     type="text"
                     placeholder="Nombre"
                     required
@@ -226,9 +177,9 @@ const GestionarUsuario = () => {
                     onChange={(e) => setUserEdit({ ...userEdit, nombreUsuario: e.target.value })}
                   />
                 </div>
-                <div className="col-sm-6 col-md-4">
+                <div className="col-sm-6">
                   <input
-                    className="form-control form-control-user"
+                    className="form-control"
                     type="text"
                     placeholder="Primer apellido"
                     required
@@ -236,9 +187,11 @@ const GestionarUsuario = () => {
                     onChange={(e) => setUserEdit({ ...userEdit, primerApellido: e.target.value })}
                   />
                 </div>
-                <div className="col-sm-6 col-md-4">
+              </div>
+              <div className="row mb-3">
+                <div className="col-sm-6">
                   <input
-                    className="form-control form-control-user"
+                    className="form-control"
                     type="text"
                     placeholder="Segundo apellido"
                     required
@@ -247,32 +200,8 @@ const GestionarUsuario = () => {
                   />
                 </div>
               </div>
-              <div className="row mb-3">
-  <div className="col-sm-6 mb-3 mb-sm-0">
-    <input
-      className="form-control form-control-user"
-      type="password"
-      id="password"
-      placeholder="Contraseña"
-      required
-      value={userEdit ? userEdit.contraseniaUsuario : ''}
-      onChange={(e) => setUserEdit({ ...userEdit, contraseniaUsuario: e.target.value })}
-    />
-  </div>
-  <div className="col-sm-6">
-    <input
-      className="form-control form-control-user"
-      type="password"
-      id="verifyPassword"
-      placeholder="Confirmar contraseña"
-      required
-      value={userEdit ? userEdit.verifyPassword : ''}
-      onChange={(e) => setUserEdit({ ...userEdit, verifyPassword: e.target.value })}
-    />
-  </div>
-</div>
               <Button variant="primary" type="submit">
-                {userEdit ? 'Actualizar Usuario' : 'Registrar Usuario'}
+                Guardar Datos
               </Button>
             </form>
           </Modal.Body>
@@ -280,81 +209,80 @@ const GestionarUsuario = () => {
             <Button variant="secondary" onClick={handleCloseModal}>Cerrar</Button>
           </Modal.Footer>
         </Modal>
+
+        {/* Modal de Cambio de Contraseña */}
+        <Modal show={showPasswordModal} onHide={handleClosePasswordModal}>
+          <Modal.Header closeButton>
+            <Modal.Title>Cambiar Contraseña</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <form onSubmit={(e) => {
+              e.preventDefault();
+              actualizarContrasena(userEdit.idUsuario);
+            }}>
+              <div className="mb-3">
+                <input
+                  className="form-control"
+                  type="password"
+                  placeholder="Nueva Contraseña"
+                  required
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+              <div className="mb-3">
+                <input
+                  className="form-control"
+                  type="password"
+                  placeholder="Confirmar Contraseña"
+                  required
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                />
+              </div>
+              <Button variant="primary" type="submit">
+                Cambiar Contraseña
+              </Button>
+            </form>
+          </Modal.Body>
+        </Modal>
+
         <ToastContainer />
-        <div className="col-md-10 search-table-col" style={{ paddingTop: '0px', marginTop: '50px', marginLeft: '50px' }}>
-          <div className="form-group pull-right col-lg-4">
-            <input
-              type="text"
-              className="search form-control"
-              placeholder="Buscar por nombre"
-              value={search}
-              onChange={handleSearchChange}
-            />
-          </div>
-          <div className="table-responsive table table-hover table-bordered results">
-            <table className="table table-hover table-bordered">
-              <thead className="bill-header cs">
-                <tr>
-                  
-                  <th className="col-lg-2">Nombre</th>
-                  <th className="col-lg-3">Primer apellido</th>
-                  <th className="col-lg-2">Segundo apellido</th>
-                  <th className="col-lg-2">Correo</th>
-                  <th className="col-lg-2">Acción</th>
-                </tr>
-              </thead>
-              <tbody>
-                {filteredUsers.length === 0 ? (
-                  <tr className="warning no-result">
-                    <td colSpan="6" className="text-center">
-                      <FaExclamationTriangle /> No Result !!!
-                    </td>
-                  </tr>
-                ) : (
-                  filteredUsers.map((user, index) => (
-                    <tr key={user.idUsuario}>          
-                      <td>{user.nombreUsuario}</td>
-                      <td>{user.primerApellido}</td>
-                      <td>{user.segundoApellido}</td>
-                      <td>{user.correoUsuario}</td>
-                      <td>
-                        <button
-                          className="btn btn-warning btn-sm me-2"
-                          type="button"
-                          onClick={() => handleShowModal(user)}
-                        >
-                          <FontAwesomeIcon icon={faEdit} style={{ fontSize: '15px' }} />
-                        </button>
-                        <button
-                          className="btn btn-danger btn-sm"
-                          type="button"
-                          onClick={() => handleDelete(user.idUsuario)}
-                        >
-                          <FaTrash style={{ fontSize: '15px' }} />
-                        </button>
-                      </td>
-                    </tr>
-                  ))
-                )}
-              </tbody>
-            </table>
-          </div>
-          <div className="d-flex justify-content-between align-items-center mt-3">
-            <button className="btn btn-secondary" onClick={() => window.history.back()}>
-              Volver
-            </button>
-            <nav>
-              <ul className="pagination">
-                {[...Array(totalPages)].map((_, index) => (
-                  <li key={index} className={`page-item ${currentPage === index + 1 ? 'active' : ''}`}>
-                    <button onClick={() => paginate(index + 1)} className="page-link">
-                      {index + 1}
+
+        {/* Tabla de Usuarios */}
+        <div className="table-responsive mt-5">
+          <table className="table table-hover table-bordered">
+            <thead>
+              <tr>
+                <th>Nombre</th>
+                <th>Primer apellido</th>
+                <th>Segundo apellido</th>
+                <th>Correo</th>
+                <th>Acción</th>
+              </tr>
+            </thead>
+            <tbody>
+              {filteredUsers.map(user => (
+                <tr key={user.idUsuario}>
+                  <td>{user.nombreUsuario}</td>
+                  <td>{user.primerApellido}</td>
+                  <td>{user.segundoApellido}</td>
+                  <td>{user.correoUsuario}</td>
+                  <td>
+                    <button className="btn btn-warning btn-sm me-2" onClick={() => handleShowModal(user)}>
+                      <FontAwesomeIcon icon={faEdit} />
                     </button>
-                  </li>
-                ))}
-              </ul>
-            </nav>
-          </div>
+                    <button className="btn btn-danger btn-sm me-2" onClick={() => handleDelete(user.idUsuario)}>
+                      <FaTrash />
+                    </button>
+                    <button className="btn btn-info btn-sm" onClick={() => handleShowPasswordModal(user)}>
+                      <FaKey />
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
