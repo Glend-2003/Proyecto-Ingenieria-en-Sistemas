7aae9225119dff301008d548a0ceda71450c67a2
Jamel-Travieso <jsandi12199@gmail.com>
2025-03-02 22:32:36 -0600

    Reapply "Agregación de tabla nueva llamada tbcodigoverificacion para el olvido de la contra"
    This reverts commit a4626bb2b1f6d3232e7f3cbfdbc8df130b73979e.

---
+++ carniceria/src/main/java/com/bendicion/la/carniceria/carniceria/domain/CodigoVerificacion.java
@@ -0,0 +1,80 @@
+package com.bendicion.la.carniceria.carniceria.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
+
+/**
+ *
+ * @author Jamel Sandí
+ */
+
+@Entity
+@Table(name = "tbcodigoverificacion")
+public class CodigoVerificacion {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idCodigo")
+    private int idCodigo;
+    
+    @Column(name = "numCodigo")
+    private String numCodigo;
+    
+    @Column(name = "fechaExpiracion")
+    private LocalDate fechaExpiracion;
+    
+    @OneToOne
+    @JoinColumn(name = "idUsuario")
+    private Usuario usuario;
+
+    public CodigoVerificacion(int idCodigo, String numCodigo, LocalDate fechaExpiracion, Usuario usuario) {
+        this.idCodigo = idCodigo;
+        this.numCodigo = numCodigo;
+        this.fechaExpiracion = fechaExpiracion;
+        this.usuario = usuario;
+    }
+
+    public CodigoVerificacion() {
+    }
+
+    public void setIdCodigo(int idCodigo) {
+        this.idCodigo = idCodigo;
+    }
+
+    public void setNumCodigo(String numCodigo) {
+        this.numCodigo = numCodigo;
+    }
+
+    public void setFechaExpiracion(LocalDate fechaExpiracion) {
+        this.fechaExpiracion = fechaExpiracion;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public int getIdCodigo() {
+        return idCodigo;
+    }
+
+    public String getNumCodigo() {
+        return numCodigo;
+    }
+
+    public LocalDate getFechaExpiracion() {
+        return fechaExpiracion;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+        
+}
