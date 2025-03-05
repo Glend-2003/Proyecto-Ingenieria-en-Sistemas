773aba023c2b2ecd01458a6e582cf91c0bcfff4a
Glend <115124841+Glend-2003@users.noreply.github.com>
2025-03-04 18:47:24 -0600

    Cambios alertas

--- front/src/components/Login/LoginApp.js
+++ front/src/components/Login/LoginApp.js
@@ -1,15 +1,16 @@
-// App.js
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles.min.css';
-import { Offcanvas, Navbar, Container, Nav, Button, ListGroup, Badge, Card} from 'react-bootstrap';
-import Carrito from '../Carrito/CarritoApp.js'; 
+import { Offcanvas, Navbar, Container, Nav, Button, ListGroup, Badge, Card } from 'react-bootstrap';
+import Carrito from '../Carrito/CarritoApp.js';
 import ListaProductosApp from '../Catalogo/ListaProductosApp.js';
 import DropDown from "../DropDown/DropDown";
 import { toast } from 'react-toastify';
 import './Login.css';
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
 
 function App() {
   const [loginData, setLoginData] = useState({
@@ -26,7 +27,52 @@ function App() {
   const [cart, setCart] = useState([]);
   const [productos, setProductos] = useState([]);
 
+  // Definir el componente Alert para Snackbar
+  const Alert = React.forwardRef(function Alert(props, ref) {
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+  });
 
+  // Estados para manejar el Snackbar
+  const [openSnackbar, setOpenSnackbar] = useState(false);
+  const [snackbarMessage, setSnackbarMessage] = useState('');
+  const [snackbarSeverity, setSnackbarSeverity] = useState('success'); // 'success', 'error', 'warning', 'info'
+
+  // Función para abrir el Snackbar
+  const handleOpenSnackbar = (message, severity) => {
+    
+    setSnackbarMessage(message);
+    setSnackbarSeverity(severity);
+    setOpenSnackbar(true);
+  };
+
+  // Función para cerrar el Snackbar
+  const handleCloseSnackbar = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setOpenSnackbar(false);
+  };
+
+  // Función para manejar el cambio en los inputs del formulario
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setLoginData({
+      ...loginData,
+      [name]: value,
+    });
+  };
+
+  // Función para mostrar/ocultar el sidebar
+  const handleShowSidebar = () => {
+    setShowSidebar(!showSidebar && !idUsuario);
+  };
+
+  // Función para mostrar/ocultar el carrito
+  const handleShowCart = () => {
+    setShowCart(!showCart);
+  };
+
+  // Función para agregar productos al carrito
   const addToCart = (producto) => {
     setCart((prevCart) => {
       const existingItem = prevCart.find(item => item.idProducto === producto.idProducto);
@@ -41,53 +87,8 @@ function App() {
       }
     });
   };
-  
-  const addToCart2 = (producto) => {
-   
-    const userCart = {
-      ...producto,
-      usuarioId: idUsuario, // Asociamos el carrito con el idUsuario
-    };
-  
-    setCart((prevCart) => [...prevCart, userCart]);
 
-  };
-
-  useEffect(() => {
-    // Verificar si el idUsuario del carrito es diferente del logueado
-    if (cart.length > 0 && cart[0].usuarioId !== idUsuario) {
-     
-      setCart([]);
-      localStorage.setItem("carrito", JSON.stringify([]));
-      
-    } else {
-      localStorage.setItem("carrito", JSON.stringify(cart));
-    }
-
-    cargarProductos();
-  }, [cart, idUsuario]);
-
-  const cargarProductos = async () => {
-    try {
-      const response = await axios.get("http://localhost:8080/producto/");
-      setProductos(response.data);
-    } catch (error) {
-      console.error("Error al cargar productos:", error);
-      toast.error("Ocurrió un error al cargar los productos");
-    }
-};
-
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setLoginData({
-      ...loginData,
-      [name]: value,
-    });
-  };
-
-  const handleShowSidebar = () => setShowSidebar(!showSidebar && !idUsuario);
-  const handleShowCart = () => setShowCart(!showCart);
-
+  // Función para eliminar productos del carrito
   const removeFromCart = (indexToRemove) => {
     const updatedCart = cart.filter((_, idx) => idx !== indexToRemove);
     setCart(updatedCart);
@@ -95,9 +96,7 @@ function App() {
     toast.info("Producto eliminado del carrito");
   };
 
-  const removeFromCart2 = (indexToRemove) => {
-    setCart((prevCart) => prevCart.filter((_, idx) => idx !== indexToRemove));
-  };
+  // Resto del código...
 
   const login = () => {
     axios
@@ -110,9 +109,11 @@ function App() {
           localStorage.setItem('nombreRol', response.data.rol.nombreRol);
           localStorage.setItem('idUsuario', response.data.idUsuario);
 
-          setLoginStatus(
-            'Login exitoso. Bienvenido ' + response.data.nombreUsuario
-          );
+          
+          // Mostrar Snackbar de éxito
+          handleOpenSnackbar("Bienvenido a Carnoiceria La Bendición.", "success");
+
+          setLoginStatus('Login exitoso. Bienvenido ' + response.data.nombreUsuario);
 
           if (
             response.data.rol.nombreRol === 'Administrador' ||
@@ -120,20 +121,15 @@ function App() {
             response.data.rol.nombreRol === 'Gerente'
           ) {
             navigate('/principal');
-          } 
+          }
 
-           if (
-            
-            response.data.rol.nombreRol === 'Usuario' 
-          ) {
+          if (response.data.rol.nombreRol === 'Usuario') {
             navigate('/');
             setShowSidebar(false);
-          } 
-          
-         else {
-            setLoginStatus('Rol no reconocido');
           }
         } else {
+          // Mostrar Snackbar de error
+          handleOpenSnackbar("Error en el ingreso, verificar datos!", "error");
           setLoginStatus('Credenciales incorrectas');
         }
       })
@@ -142,6 +138,8 @@ function App() {
           'Error en el login:',
           error.response ? error.response.data : error.message
         );
+        // Mostrar Snackbar de error
+        handleOpenSnackbar("Error en el ingreso, "+ error.response.data, "error");
         setLoginStatus('Error en el servidor o en las credenciales');
       });
   };
@@ -151,8 +149,6 @@ function App() {
     login();
   };
 
-  
-  
   return (
     <>
       {/* Navbar */}
@@ -161,19 +157,15 @@ function App() {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-            <div
-          
-            >
-              <Nav.Link onClick={handleShowSidebar}>
-
-              <DropDown icon = {<svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" fill="currentColor" className="bi bi-person" viewBox="0 0 16 16">
-                  <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664z" />
-                </svg>} idUsuario={idUsuario}/>
-                
-              </Nav.Link>
+              <div>
+                <Nav.Link onClick={handleShowSidebar}>
+                  <DropDown icon={<svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" fill="currentColor" className="bi bi-person" viewBox="0 0 16 16">
+                    <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664z" />
+                  </svg>} idUsuario={idUsuario} />
+                </Nav.Link>
               </div>
               <Nav.Link onClick={handleShowCart}>
-              <svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" fill="currentColor" className="bi bi-cart" viewBox="0 0 16 16">
+                <svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" fill="currentColor" className="bi bi-cart" viewBox="0 0 16 16">
                   <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l1.313 7h8.17l1.313-7zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2" />
                 </svg>
                 {cart.length > 0 && <Badge bg="danger">{cart.length}</Badge>}
@@ -182,7 +174,7 @@ function App() {
           </Navbar.Collapse>
         </Container>
       </Navbar>
-       
+
       {/* Offcanvas Sidebar */}
       <Offcanvas show={showSidebar} onHide={handleShowSidebar} placement="end">
         <Offcanvas.Header closeButton>
@@ -244,9 +236,21 @@ function App() {
       <ListaProductosApp addToCart={addToCart} />
 
       {/* Carrito */}
-      <Carrito showCart={showCart} handleShowCart={handleShowCart} cart={cart} 
-        removeFromCart={removeFromCart} 
+      <Carrito showCart={showCart} handleShowCart={handleShowCart} cart={cart}
+        removeFromCart={removeFromCart}
       />
+
+      {/* Snackbar para mostrar alertas */}
+      <Snackbar
+        open={openSnackbar}
+        autoHideDuration={6000} // Duración en milisegundos
+        onClose={handleCloseSnackbar}
+        anchorOrigin={{ vertical: 'top', horizontal: 'right' }} // Posición del Snackbar
+      >
+        <Alert onClose={handleCloseSnackbar} severity={snackbarSeverity} sx={{ width: '100%' }}>
+          {snackbarMessage}
+        </Alert>
+      </Snackbar>
     </>
   );
 }
