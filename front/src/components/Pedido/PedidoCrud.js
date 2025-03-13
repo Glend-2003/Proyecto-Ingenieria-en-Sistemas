250126082a4df2513c0927257e0d2c1a8d2646e6
deyCedeno <162640178+deyCedeno@users.noreply.github.com>
2025-03-13 05:01:35 -0600

    Revert "12/03/25"
    This reverts commit edc07a5d48ddb8e8d1e4268fed4838debb71c94a.

--- front/src/components/Pedido/PedidoCrud.js
+++ front/src/components/Pedido/PedidoCrud.js
@@ -1,17 +1,26 @@
-// PedidoCrud.js ajustado para mejor uso del espacio
 import React from 'react';
 import { Container, Row, Col, Card } from 'react-bootstrap';
 
-function PedidoCrud() {
+function Pedido() {
   return (
-
-    <div>
-      <h1>¡Hola Mundo desde la página de Pedidos!</h1>
-      
-    </div>
+    <Container className="py-5">
+      <Row className="justify-content-center">
+        <Col md={8}>
+          <Card>
+            <Card.Header as="h2" className="text-center bg-primary text-white">
+              Finalizar Pedido
+            </Card.Header>
+            <Card.Body>
+              <h3 className="mb-4 text-center">¡Hola Mundo desde la página de Pedido!</h3>
+              <p className="text-center">
+                Aquí podrás completar la información para finalizar tu compra.
+              </p>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   );
-
-    
 }
 
-export default PedidoCrud;
+export default Pedido;
