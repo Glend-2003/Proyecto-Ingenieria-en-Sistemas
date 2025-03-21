5011df1ce012d078d16a26f6a6a656b44a43f5c4
Glend <115124841+Glend-2003@users.noreply.github.com>
2025-03-20 20:16:43 -0600

    20/03/2025
    Filtrado terminado

--- front/src/paginas/ResPagina.js
+++ front/src/paginas/ResPagina.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ListaProductosApp from '../components/Catalogo/ListaProductosApp';
 
 const ResPagina = () => {
+  useEffect(() => {
+    console.log("Página de cortes de res cargada");
+  }, []);
+
   return (
     <div>
-      <h1>Cortes de Res</h1>
+      <h1 className="text-center my-4">Cortes de Res</h1>
       <ListaProductosApp categoria="Res" />
     </div>
   );
