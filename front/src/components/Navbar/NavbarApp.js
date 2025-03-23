f5fd33f4b6a67fd79b5a404295f103319e43b049
Glend <115124841+Glend-2003@users.noreply.github.com>
2025-03-23 14:14:22 -0600

    23/03/25
    Arreglo de icono de busqueda

--- front/src/components/Navbar/NavbarApp.js
+++ front/src/components/Navbar/NavbarApp.js
@@ -126,6 +126,33 @@ const NavbarApp = () => {
               </svg>
               {cart.length > 0 && <Badge bg="danger">{cart.length}</Badge>}
             </Nav.Link>
+            <Nav.Link onClick={() => setSearchOpen(!searchOpen)}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="1.5em"
+          height="1.5em"
+          fill="currentColor"
+          className="bi bi-search"
+          viewBox="0 0 16 16"
+        >
+          <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.099zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+        </svg>
+        
+      </Nav.Link>
+
+      {searchOpen && (
+        <Form className="d-flex">
+          <InputGroup>
+            <FormControl
+              type="text"
+              placeholder="Buscar..."
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+              autoFocus
+            />
+          </InputGroup>
+        </Form>
+      )}
           </Nav>
         </BootstrapNavbar.Collapse>
       </Container>
