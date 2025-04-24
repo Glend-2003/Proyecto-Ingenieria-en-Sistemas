126d773032e6a3cc485b54e6a4fa7b2e046e03ed
Glend <115124841+Glend-2003@users.noreply.github.com>
2025-04-24 11:48:22 -0600

    24/04/2025
    Eliminacion de idProducto en tbCarrito

--- carniceria/src/main/java/com/bendicion/la/carniceria/carniceria/domain/Carrito.java
+++ carniceria/src/main/java/com/bendicion/la/carniceria/carniceria/domain/Carrito.java
@@ -4,6 +4,8 @@
  */
 package com.bendicion.la.carniceria.carniceria.domain;
 
+import java.math.BigDecimal;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -12,7 +14,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import java.math.BigDecimal;
 
 /**
  *
@@ -34,10 +35,6 @@ public class Carrito {
     @ManyToOne
     @JoinColumn(name="idUsuario", nullable =false)
     private Usuario usuario;
-    
-    @ManyToOne
-    @JoinColumn(name="idProducto", nullable =false)
-    private Producto producto;
     
     @Column(name = "montoTotalCarrito")
     private BigDecimal montoTotalCarrito;
