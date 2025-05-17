6b59b3a4a24069a913db88b79508836dd2067071
DilanGutierrez <138705122+DilanGOD02@users.noreply.github.com>
2025-05-17 14:12:39 -0600

    Actualziar Stock (Corregis Actualizar Productos)

--- carniceria/src/main/java/com/bendicion/la/carniceria/carniceria/service/ICarritoProductoService.java
+++ carniceria/src/main/java/com/bendicion/la/carniceria/carniceria/service/ICarritoProductoService.java
@@ -5,17 +5,18 @@ import com.bendicion.la.carniceria.carniceria.domain.CarritoProducto;
 import jakarta.transaction.Transactional;
 
 public interface ICarritoProductoService {
+
     @Transactional
     public CarritoProducto addProductoAlCarrito(CarritoProducto carritoProducto);
-    
+
     @Transactional
     public CarritoProducto updateStock(CarritoProducto carritoProducto);
-    
+
     @Transactional
     public CarritoProducto updateProductoEnCarrito(CarritoProducto carritoProducto);
-    
+
     public List<CarritoProducto> getProductosEnCarrito(int idCarrito);
-    
+
     @Transactional
     public boolean removeProductoDeCarrito(int idCarritoProducto);
 }
